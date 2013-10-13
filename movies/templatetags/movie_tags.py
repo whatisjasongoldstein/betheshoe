5860d5f5687cb875d574325218783249ac06128f
Jason Goldstein <jason@betheshoe.com>
2013-10-13 12:21:02 -0400

    Fin' blog pages.

--- movies/templatetags/movie_tags.py
+++ movies/templatetags/movie_tags.py
@@ -6,3 +6,12 @@ from ..models import Movie
 @register.assignment_tag
 def get_movies():
     return Movie.objects.filter(publish=True).order_by('-year')
+
+@register.assignment_tag
+def get_related_movie(text):
+    """Give a big block of text, return the movie mentioned first."""
+    titles = Movie.objects.filter(publish=True).values_list("title", flat=True)
+    for title in titles:
+        if title in text:
+            return Movie.objects.get(title=title)
+    return None
