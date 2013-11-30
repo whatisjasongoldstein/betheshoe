a74c35766c81e78a931c8be91f4c351fcd7cf007
Jason Goldstein <jason@betheshoe.com>
2013-11-30 16:18:14 -0500

    Change hosts so we can test social logins.

--- betheshoe/settings.py
+++ betheshoe/settings.py
@@ -38,8 +38,8 @@ if 'test' in sys.argv:
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = [
     "betheshoe.com",
+    "preview.betheshoe.com",
     "local.betheshoe.com",
-    "bts.scruffylogic.com",
     "127.0.0.1",
 ]
 
