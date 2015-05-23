dc63ff8cb1a68ed241857f3c297bfc7e79ba8dc5
Jason Goldstein <jason@betheshoe.com>
2015-05-23 17:25:09 -0400

    Use Django Pipeline Helpers

---
+++ betheshoe/assets.py
@@ -0,0 +1,34 @@
+PIPELINE_CSS = {
+    'betheshoe_styles': {
+        'source_filenames': (
+            "css/project.less",
+            "css/splash.less",
+            "css/player.less",
+            "css/movies.less",
+            "css/featured.less",
+            "css/blog.less",
+            "css/about.less",
+            "css/tooltip.less",
+            "css/font-awesome.min.css",
+        ),
+        'output_filename': 'css/app.css',
+        'extra_context': {
+            'media': 'screen,projection',
+        },
+    },
+}
+
+PIPELINE_JS = {
+    'betheshoe_js': {
+        'source_filenames': (
+            "js/jquery.js",
+            "js/underscore.js",
+            "js/underscore.string.js",
+            "js/modernizer.2.6.2.js",
+            "js/tooltip.js",
+            "js/project.js",
+            "js/deferred-images.js",
+        ),
+        'output_filename': 'js/app.js',
+    }
+}
--- betheshoe/settings.py
+++ betheshoe/settings.py
@@ -267,43 +267,12 @@ MARKDOWN_DEUX_STYLES = {
 }
 
 
-# PIPELINE = True
 PIPELINE_DISABLE_WRAPPER = True
 
-PIPELINE_CSS = {
-    'betheshoe_styles': {
-        'source_filenames': (
-            "css/project.less",
-            "css/splash.less",
-            "css/player.less",
-            "css/movies.less",
-            "css/featured.less",
-            "css/blog.less",
-            "css/about.less",
-            "css/tooltip.less",
-            "css/font-awesome.min.css",
-        ),
-        'output_filename': 'css/app.css',
-        'extra_context': {
-            'media': 'screen,projection',
-        },
-    },
-}
+import pipeline_helpers
+PIPELINE_CSS = pipeline_helpers.find_css()
+PIPELINE_JS = pipeline_helpers.find_js()
 
-PIPELINE_JS = {
-    'betheshoe_js': {
-        'source_filenames': (
-            "js/jquery.js",
-            "js/underscore.js",
-            "js/underscore.string.js",
-            "js/modernizer.2.6.2.js",
-            "js/tooltip.js",
-            "js/project.js",
-            "js/deferred-images.js",
-        ),
-        'output_filename': 'js/app.js',
-    }
-}
 
 PIPELINE_COMPILERS = (
     'pipeline.compilers.less.LessCompiler',
